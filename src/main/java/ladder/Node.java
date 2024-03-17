9e61b44ef197bfa849cce7b307623a84549d0c25
kamothi <rhkddlf7911@naver.com>
2024-03-17 13:38:08 +0900

    Feat: direction getter 추가

--- src/main/java/ladder/Node.java
+++ src/main/java/ladder/Node.java
@@ -26,6 +26,7 @@ public class Node {
         return ladderDirection == LadderDirection.RIGHT;
     }
 
-
-
+    public LadderDirection getLadderDirection() {
+        return ladderDirection;
+    }
 }
