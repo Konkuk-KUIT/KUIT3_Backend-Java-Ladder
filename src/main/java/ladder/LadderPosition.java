edf6e97846556619881f01a05597eb3c93791439
kamothi <rhkddlf7911@naver.com>
2024-03-17 13:39:39 +0900

    Feat: 현재 위치를 위한 LadderPosition 분리

--- src/main/java/ladder/LadderPosition.java
+++ src/main/java/ladder/LadderPosition.java
@@ -1,2 +1,39 @@
-package ladder;public class LadderPosition {
+package ladder;
+
+import java.util.Objects;
+
+public class LadderPosition {
+    private final Position row;
+    private final Position col;
+
+    private LadderPosition(Position row, Position col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public Position getRow() {
+        return row;
+    }
+
+    public Position getCol() {
+        return col;
+    }
+
+    public static LadderPosition fromCoordinate(Position row, Position col){
+        return new LadderPosition(row,col);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        LadderPosition other = (LadderPosition) obj;
+        return Objects.equals(row, other.row) && Objects.equals(col, other.col);
+    }
+
+
 }
