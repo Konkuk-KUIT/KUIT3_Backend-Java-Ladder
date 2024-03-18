d8134c4248e4279b216be2bcfc63d06ccac84089
david-parkk <ji0513ji@naver.com>
2024-03-19 02:00:21 +0900

    feat: Pipe class & refactor: Row class

--- src/main/java/ladder/Row.java
+++ src/main/java/ladder/Row.java
@@ -2,65 +2,54 @@ package ladder;
 
 import ladder.util.CustomException;
 import ladder.util.ErrorCode;
-import ladder.util.RowConstant;
+import ladder.util.PipeConstant;
+import ladder.util.PipeStatus;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Row {
-    private int[] row;
+    private Pipe[] row;
 
     public Row(int numberOfPerson) {
         validateNumberOfPerson(numberOfPerson);
-        row = new int[numberOfPerson];
+        row= new Pipe[numberOfPerson];
+        IntStream.range(0, numberOfPerson).forEach(i -> row[i] = new Pipe());
+
     }
 
     public void drawLine(int lineStartPosition) {
         validateDrawLinePosition(lineStartPosition);
-        row[lineStartPosition] = RowConstant.RIGHT_DRAW_CONSTANT;
-        row[lineStartPosition + RowConstant.ROW_BLOCK_SIZE] = RowConstant.LEFT_DRAW_CONSTANT;
+
+        row[lineStartPosition].setPipeStatusRight();
+        row[lineStartPosition + PipeConstant.ROW_BLOCK_SIZE].setPipeStatusLeft();
     }
 
     public int nextPosition(int position) {
 
         validatePosition(position);
-
-        if (isLeft(position)) {
-            return position + RowConstant.LEFT_DRAW_CONSTANT;
-        }
-        if (isRight(position)) {
-            return position + RowConstant.RIGHT_DRAW_CONSTANT;
-        }
-
-        return position;
+        return position+row[position].getMoveWeight();
     }
 
-    private boolean isLeft(int position) {
-        return row[position] == RowConstant.LEFT_DRAW_CONSTANT;
-    }
-
-    private boolean isRight(int position) {
-        return row[position] == RowConstant.RIGHT_DRAW_CONSTANT;
-    }
 
     private void validateNumberOfPerson(int numberOfPerson) {
         if(numberOfPerson < 1) {
-
-            //throw new IllegalArgumentException("게임의 참여자 수는 1명 이상이어야 합니다.");
             throw new CustomException(ErrorCode.LACK_PARTICIPANTS);
         }
     }
 
     private void validateDrawLinePosition(int lineStartPosition) {
-        if(lineStartPosition < RowConstant.START_INDEX || lineStartPosition >= row.length - 1 || row[lineStartPosition] == RowConstant.LEFT_DRAW_CONSTANT || row[lineStartPosition + RowConstant.ROW_BLOCK_SIZE] == RowConstant.RIGHT_DRAW_CONSTANT) {
+        if(lineStartPosition < PipeConstant.START_INDEX || lineStartPosition >= row.length - 1 ||
+                row[lineStartPosition].isLeftExtended()||row[lineStartPosition+PipeConstant.ROW_BLOCK_SIZE].isRightExtended()) {
             throw new CustomException(ErrorCode.INVALID_LINE_POSITION);
         }
     }
 
     private void validatePosition(int position) {
-        if(position >= row.length || position < RowConstant.START_INDEX ) {
+        if(position >= row.length || position < PipeConstant.START_INDEX ) {
             throw new CustomException(ErrorCode.INVALID_POSITION);
         }
     }
-
-
 
 
 }
---
+++ src/main/java/ladder/util/PipeStatus.java
@@ -0,0 +1,15 @@
+package ladder.util;
+
+public enum PipeStatus {
+    Left(PipeConstant.LEFT_DRAW_CONSTANT), Right(PipeConstant.RIGHT_DRAW_CONSTANT), Nothing(PipeConstant.NOTHING);
+
+    private int weight;
+
+    PipeStatus(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
