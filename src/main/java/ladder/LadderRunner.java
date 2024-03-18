92c550b825c726f2a7970c1b2cf0683f340734ba
kamothi <rhkddlf7911@naver.com>
2024-03-18 18:38:41 +0900

    Refact: printLadderLine 반환값 변경 void -> StringBuilder

--- src/main/java/ladder/LadderRunner.java
+++ src/main/java/ladder/LadderRunner.java
@@ -11,17 +11,18 @@ public class LadderRunner {
         for(int i = 0; i < rows.length; i++) {
             printLadderLine(LadderPosition.fromCoordinate(Position.fromValue(i),position));
             position = rows[i].nextPosition(position);
-            printLadderLine(LadderPosition.fromCoordinate(Position.fromValue(i),position));
+            StringBuilder sb= printLadderLine(LadderPosition.fromCoordinate(Position.fromValue(i),position));
+            System.out.println(sb);
         }
 
         return position.getPosition();
     }
 
-    public void printLadderLine(LadderPosition ladderPosition){
+    public StringBuilder printLadderLine(LadderPosition ladderPosition){
+        StringBuilder sb = new StringBuilder();
         for(int i=0;i< rows.length;i++){
-            StringBuilder sb = new StringBuilder();
             rows[i].printRow(sb,Position.fromValue(i),ladderPosition);
-            System.out.println(sb);
         }
+        return sb;
     }
 }
