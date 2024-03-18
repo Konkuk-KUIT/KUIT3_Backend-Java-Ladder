e6d6820d58fddaddef8db42f6f06d81137a8fb49
kamothi <rhkddlf7911@naver.com>
2024-03-18 18:47:03 +0900

    Feat: game play 기능 추가

--- src/main/java/ladder/game/LadderGame.java
+++ src/main/java/ladder/game/LadderGame.java
@@ -1,12 +1,38 @@
 package ladder.game;
 
+import ladder.LadderRunner;
+import ladder.Position;
 import ladder.creator.LadderCreator;
+import view.InputView;
+import view.OutputView;
 
 public class LadderGame {
     private final LadderCreator ladderCreator;
-    public LadderGame(LadderCreator ladderCreator){
+    private final OutputView outputView;
+    private final InputView inputView;
+    public LadderGame(LadderCreator ladderCreator,OutputView outputView, InputView inputView){
         this.ladderCreator = ladderCreator;
+        this.outputView = outputView;
+        this.inputView = inputView;
     }
 
+    public void play(){
 
+
+        // 여기에 어느 곳에 선을 그을지 확인
+        ladderCreator.drawLine(0,1);
+        ladderCreator.drawLine(1,1);
+
+        // 원하는 번호 입력
+        outputView.printSelectNumber();
+//        int number = inputView.readLadderNumber();
+        int number = 1;
+
+        // runner로 돌리기
+        LadderRunner ladderRunner = new LadderRunner(ladderCreator.getRows());
+        Position position = new Position(number);
+        int destination = ladderRunner.run(position);
+        System.out.println("도착지는?");
+        System.out.println(destination);
+    }
 }
