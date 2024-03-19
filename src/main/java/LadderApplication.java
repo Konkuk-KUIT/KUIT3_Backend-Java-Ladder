55da1a4bf0c390e67199ca6e8d875c6c36670cb4
kamothi <rhkddlf7911@naver.com>
2024-03-19 18:07:08 +0900

    Feat: 실행 Appication 추가

--- src/main/java/LadderApplication.java
+++ src/main/java/LadderApplication.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class LadderApplication {
+import ladder.game.LadderGame;
+import ladder.game.LadderGameFactory;
+import ladder.model.LadderSize;
+
+public class LadderApplication {
+    public static void main(String[] args) {
+        LadderSize ladderSize = new LadderSize(3,3);
+        LadderGame ladderGame = LadderGameFactory.randomLadderGame(ladderSize);
+        ladderGame.play(1);
+    }
 }
