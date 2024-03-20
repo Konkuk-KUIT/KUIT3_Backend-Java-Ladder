956e6949fab0e4e7a660bcd86af76d9e5c21ebeb
twkwon0417 <kwontony123@konkuk.ac.kr>
2024-03-20 21:54:03 +0900

    test: Node 단위 테스트

--- src/test/java/ladder/NodeTest.java
+++ src/test/java/ladder/NodeTest.java
@@ -1,20 +1,54 @@
 package ladder;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class NodeTest {
 
-    @Test
-    void 디렉션이_같은_두_노드의_비교() {
-        Assertions.assertEquals(Node.from(Direction.LEFT), Node.from(Direction.LEFT));
+    @ParameterizedTest
+    @EnumSource(value = Direction.class, names = {"LEFT", "RIGHT"})
+    @DisplayName("같은 Direction일 경우 두 객체는 같다 처리")
+    void sameDirectionSameObject(Direction direction) {
+        // given
+
+        // when
+        Node node1 = Node.from(direction);
+        Node node2 = Node.from(direction);
+
+        // then
+        Assertions.assertEquals(node1, node2);
     }
 
     @Test
-    void move_left_test() {
+    @DisplayName("오른쪽 노드일 경우 position + 1")
+    void rightNodeMoveRight() {
+        // given
+        Node node = Node.from(Direction.RIGHT);
+        Position position = Position.initFrom(3, NaturalNumber.from(5));
+
+        // when
+        Position movedPosition = node.move(position);
+
+        // then
+        Assertions.assertEquals(movedPosition.get(), 4);
+    }
+
+    @Test
+    @DisplayName("왼쪽 노드일 경우 position - 1")
+    void leftNodeMoveLeft() {
+        // given
         Node node = Node.from(Direction.LEFT);
         Position position = Position.initFrom(3, NaturalNumber.from(5));
+
+        // when
         Position movedPosition = node.move(position);
+
+        // then
         Assertions.assertEquals(movedPosition.get(), 2);
     }
 }
