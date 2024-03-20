34bc701cc31bcc93b3ce9b1e801bf49c0e019bfa
김민석 <skyfox001015@naver.com>
2024-03-21 02:23:35 +0900

    refactor : Node 클래스 추가

--- src/main/java/ladder/Node.java
+++ src/main/java/ladder/Node.java
@@ -1,2 +1,33 @@
-package ladder;public class Node {
+package ladder;
+
+public class Node {
+    private  Direction direction;
+
+    private Node(Direction direction) {
+        this.direction = direction;
+    }
+
+    public static Node of(Direction direction) {
+        return new Node(direction);
+    }
+
+
+    public Position move(Position position) {
+
+        if (isRight()) {
+            return position.next();
+        }
+        if (isLeft()) {
+            return position.prev();
+        }
+        return position;
+    }
+    private boolean isLeft() {
+        return direction == Direction.LEFT;
+    }
+
+    private boolean isRight( ) {
+        return direction == Direction.RIGHT;
+    }
+
 }
