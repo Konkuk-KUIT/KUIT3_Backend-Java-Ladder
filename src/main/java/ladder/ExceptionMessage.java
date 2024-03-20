934a7922ced3794d525963b826f430a2f9aa6725
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-03-20 17:02:41 +0900

    Refactor: Enum 적용

---
+++ src/main/java/ladder/ExceptionMessage.java
@@ -0,0 +1,19 @@
+package ladder;
+
+public enum ExceptionMessage {
+
+    INVALID_NUMBER_OF_PERSON("게임의 참여자 수는 1명 이상이어야 합니다."),
+    INVALID_LINE_POSITION("라인 생성이 불가능한 위치입니다."),
+    INVALID_POSITION("유효하지 않은 위치입니다.");
+
+    private final String message;
+
+    ExceptionMessage(String message){
+        this.message = message;
+    }
+
+    public  String getMessage(){
+        return message;
+    }
+
+}
--- src/main/java/ladder/Row.java
+++ src/main/java/ladder/Row.java
@@ -1,13 +1,13 @@
 package ladder;
 
+import static ladder.ExceptionMessage.*;
+
 public class Row {
     private int[] row;
 
-    // 방향을 나타내는 상수 정의
-    public static final int LEFT_LINE = -1;
-    public static final int RIGHT_LINE = 1;
     public static final int MINIMUM_NUMBER_OF_PERSON = 1;
     public static final int MAXIMUM_INVALID_POSITION_NUMBER = 0;
+
     public Row(int numberOfPerson) {
         validateNumberOfPerson(numberOfPerson);
         row = new int[numberOfPerson];
@@ -16,8 +16,8 @@ public class Row {
     // 사다리 행에 가로선은 그리는 역할
     public void drawLine(int lineStartPosition) {
         validateDrawLinePosition(lineStartPosition);
-        row[lineStartPosition] = RIGHT_LINE;
-        row[lineStartPosition + 1] = LEFT_LINE;
+        row[lineStartPosition] = Direction.RIGHT.getValue();
+        row[lineStartPosition + 1] = Direction.LEFT.getValue();
     }
 
     //현재위치 position에서 다음 위치를 계산해서 변환
@@ -45,19 +45,19 @@ public class Row {
 
     private void validateNumberOfPerson(int numberOfPerson) {
         if(numberOfPerson < MINIMUM_NUMBER_OF_PERSON) {
-            throw new IllegalArgumentException("게임의 참여자 수는 1명 이상이어야 합니다.");
+            throw new IllegalArgumentException(INVALID_NUMBER_OF_PERSON.getMessage());
         }
     }
 
     private void validateDrawLinePosition(int lineStartPosition) {
-        if(lineStartPosition < MAXIMUM_INVALID_POSITION_NUMBER || lineStartPosition >= row.length - 1 || row[lineStartPosition] == LEFT_LINE || row[lineStartPosition + 1] == RIGHT_LINE) {
-            throw new IllegalArgumentException("라인 생성이 불가능한 위치 입니다.");
+        if(lineStartPosition < MAXIMUM_INVALID_POSITION_NUMBER || lineStartPosition >= row.length - 1 || row[lineStartPosition] == Direction.LEFT.getValue() || row[lineStartPosition + 1] == Direction.RIGHT.getValue()) {
+            throw new IllegalArgumentException(INVALID_LINE_POSITION.getMessage());
         }
     }
 
     private void validatePosition(int position) {
         if(position >= row.length || position < MAXIMUM_INVALID_POSITION_NUMBER ) {
-            throw new IllegalArgumentException("유효하지 않은 위치 입니다.");
+            throw new IllegalArgumentException(INVALID_POSITION.getMessage());
         }
     }
 
