d929f61885a02a190d400d906a9ffc37f268d4b0
twkwon0417 <kwontony123@konkuk.ac.kr>
2024-03-20 22:38:43 +0900

    test: Position 단위 테스트 추가

--- src/test/java/ladder/PositionTest.java
+++ src/test/java/ladder/PositionTest.java
@@ -1,12 +1,49 @@
 package ladder;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PositionTest {
     @Test
-    void position_Next() {
+    @DisplayName("사람 수보다 큰 값이 포지션으로 설정되려 할 때 Exception을 던지는 테스트.")
+    void positionLargerThanNumberOfPerson() {
+        // given
+        NaturalNumber numberOfPerson = NaturalNumber.from(5);
+
+        // then
+        assertThrows(IllegalArgumentException.class, () -> {
+            Position.initOf(6, numberOfPerson);
+        });
+    }
+
+    @Test
+    @DisplayName("next를 호출했을때 증가된 포지션을 반환하는지 테스트")
+    void nextedPosition() {
+        // given
         Position position = Position.initOf(0, NaturalNumber.from(5));
-        Assertions.assertEquals(1, position.next().get());
+        Position expected = Position.initOf(1, NaturalNumber.from(5));
+
+        // when
+        Position nextPosition = position.next();
+
+        // then
+        assertEquals(expected, nextPosition);
+    }
+
+    @Test
+    @DisplayName("prev를 호출했을때 작이진 포지션을 반환하는지 테스트")
+    void prevedPosition() {
+        // given
+        Position position = Position.initOf(1, NaturalNumber.from(5));
+        Position expected = Position.initOf(0, NaturalNumber.from(5));
+
+        // when
+        Position prevPosition = position.prev();
+
+        // then
+        assertEquals(expected, prevPosition);
     }
 }
