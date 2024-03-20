c4a2abcbee10196c0cecead6b99d22a5f829fae8
twkwon0417 <kwontony123@konkuk.ac.kr>
2024-03-20 09:46:05 +0900

    test: mocking객체를 사용해 LadderRunner의 Test

--- src/test/java/ladder/LadderRunnerTest.java
+++ src/test/java/ladder/LadderRunnerTest.java
@@ -1,2 +1,31 @@
-package ladder;public class LadderRunnerTest {
+package ladder;
+
+import ladder.ladderCreator.LadderCreator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LadderRunnerTest {
+    class MockLadderCreator implements LadderCreator{
+
+        @Override
+        public Row[] create() {
+            int totalPlayer = 5;
+            Row[] rows = new Row[4];    // 높이는 4
+            for(int i = 0; i < 4; i++) {
+                rows[i] = new Row(NaturalNumber.from(totalPlayer));
+            }
+
+            rows[0].drawLine(Position.initFrom(0, NaturalNumber.from(5)));
+            rows[1].drawLine(Position.initFrom(1, NaturalNumber.from(5)));
+
+            return rows;
+        }
+    }
+
+    @Test
+    void 사다리_타기_시험() {
+        LadderRunner ladderRunner = new LadderRunner(new MockLadderCreator());
+        Position pos = ladderRunner.run(Position.initFrom(0, NaturalNumber.from(5)));
+        Assertions.assertEquals(pos.get(), 2);
+    }
 }
