dfbaf393560035ab9fda2f0297aa6cafd8471d33
twkwon0417 <kwontony123@konkuk.ac.kr>
2024-03-20 22:38:07 +0900

    fix: Position 객체의 포지션값이 같으면 equal 하도록 hashcode, equals overriding

--- src/main/java/ladder/Position.java
+++ src/main/java/ladder/Position.java
@@ -1,5 +1,7 @@
 package ladder;
 
+import java.util.Objects;
+
 public class Position {
     private final int position;
 
@@ -32,5 +34,22 @@ public class Position {
         if(position < 0 || position >= numberOfPerson.get()) {
             throw new IllegalArgumentException(ExceptionMessage.INVALID_POSITION.getErrorMessage());
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
