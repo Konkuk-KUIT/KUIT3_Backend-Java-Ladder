9e685cee514ed278d6c0054f4406778965e0e8f3
hyunn522 <tjgus52213@gmail.com>
2024-03-22 14:16:27 +0900

    feat: 테스트 코드 추가 및 ladder 클래스 분리

---
+++ src/main/java/ladder/LadderCreator.java
@@ -0,0 +1,17 @@
+package ladder;
+
+public class LadderCreator {
+    private Row[] rows;
+
+    public LadderCreator(NaturalNumber numberOfRows, NaturalNumber numberOfPerson) {
+        rows = new Row[numberOfPerson.getNum()];
+
+        for(int i = 0; i < numberOfRows.getNum(); i++) {
+            rows[i] = new Row(numberOfPerson.getNum());
+        }
+    }
+
+    public void drawLine(Position row, Position col) {
+        rows[row.getPosition()].drawLine(col.getPosition());
+    }
+}
---
+++ src/main/java/ladder/LadderRunner.java
@@ -0,0 +1,13 @@
+package ladder;
+
+public class LadderRunner {
+
+    public int run(int position) {
+
+        for(int i = 0; i < rows.length; i++) {
+            position = rows[i].nextPosition(position);
+        }
+
+        return position;
+    }
+}
